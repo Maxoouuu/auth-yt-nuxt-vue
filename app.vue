20f870b0a71667e6e8384d824cbb1c4d4d86c331
Maxime Coriton <maxime.coriton@gmail.com>
2022-12-20 17:19:34 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import { Auth } from "aws-amplify";
+/* Import styles */
+import "@aws-amplify/ui-vue/styles.css";
+
+</script>
+<template>
+  <div>
+    <Authenticator >
+      <template v-slot="{user, signOut}">
+        <h1>Hello {{ user.username }}</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </Authenticator>
+  </div>
+</template>
